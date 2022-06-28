e57efff9c2c862bb80b8edaa51cbc80cd80d3bdc
ioerger <ioerger@cs.tamu.edu>
2022-06-28 10:56:32 -0500

    added mtb_lineages_vcf.py

---
+++ mtb_lineages_vcf.py
@@ -0,0 +1,27 @@
+import sys
+
+# based on table of SNPs in Coll et al (2014). https://www.nature.com/articles/ncomms5812
+# note: just looking for matches by coordinates, not nucleotides
+
+if len(sys.argv)<3:
+  print("usage: python mtb_lineages_vcf.py <.vcf> coll_2014_LS_SNPs.txt")
+  sys.exit(0)
+
+SNPs = {}
+for line in open(sys.argv[2]): # "coll_2014_LS_SNPs.txt"
+  w = line.split()
+  SNPs[w[1]] = w
+
+lineages = []
+for line in open(sys.argv[1]): # vcf file
+  if line.startswith('#'): continue
+  w = line.rstrip().split('\t')
+  pos,kind = w[1],w[6]
+  if pos in SNPs and kind=='PASS':
+    v = SNPs[pos]
+    mut = "%s/%s" % (w[3],w[4])
+    lineage = v[0]
+    if ("**" not in lineage and mut==SNPs[pos][3]) or ("**" in lineage and mut!=SNPs[pos][3]): print("# %s" % ('\t'.join(v))); lineages.append(lineage)
+
+# the longest lineage name should be the most specific
+print(sorted(lineages,reverse=True)[0])
